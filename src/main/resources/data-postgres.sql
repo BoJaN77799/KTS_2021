7a6671a740cbc63c7b61f68b2d61e847e1346d4d
Bojan <bojanbaskalo@hotmail.rs>
2021-11-11 22:15:21 +0100

    application.properties modified

--- src/main/resources/data-postgres.sql
+++ src/main/resources/data-postgres.sql
@@ -1,5 +1,5 @@
 insert into app_user (password, first_name, last_name, email, telephone, is_password_changed, is_email_verified)
-                    values ('pericakralj', 'Petar', 'Markovic', 'perazdera2@gmail.com', '0642312341', 'False', 'False');
+                    values ('pericakralj', 'Filip', 'Markovic', 'perazdera2@gmail.com', '0642312341', 'False', 'False');
 
 
 insert into app_user (password, first_name, last_name, email, telephone, is_password_changed, is_email_verified)
