d68d8b0153fdc329b947b2c20642579a0d087152
Veljko <veljkozajecar@gmail.com>
2021-12-04 20:12:47 +0100

    Food repository integration test added to suite.

--- src/test/java/com/app/RestaurantApp/SuiteAll.java
+++ src/test/java/com/app/RestaurantApp/SuiteAll.java
@@ -1,12 +1,13 @@
 package com.app.RestaurantApp;
 
+import com.app.RestaurantApp.food.FoodRepositoryIntegrationTests;
 import com.app.RestaurantApp.users.appUser.AppUserServiceIntegrationTests;
 import com.app.RestaurantApp.users.appUser.AppUserServiceUnitTests;
 import org.junit.runner.RunWith;
 import org.junit.runners.Suite;
 
 @RunWith(Suite.class)
-@Suite.SuiteClasses({AppUserServiceUnitTests.class, AppUserServiceIntegrationTests.class})
+@Suite.SuiteClasses({AppUserServiceUnitTests.class, AppUserServiceIntegrationTests.class, FoodRepositoryIntegrationTests.class})
 public class SuiteAll {
 
 
