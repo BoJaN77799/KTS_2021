4b22b4cfa132ddca879a949b732b6d553a0fe77a
bjelicamarko <marko.bjelica19@gmail.com>
2021-12-10 22:25:17 +0100

    item tests finished.

--- src/test/java/com/app/RestaurantApp/item/ItemServiceUnitTests.java
+++ src/test/java/com/app/RestaurantApp/item/ItemServiceUnitTests.java
@@ -1,4 +1,55 @@
 package com.app.RestaurantApp.item;
 
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+@ExtendWith(SpringExtension.class)
+@SpringBootTest
 public class ItemServiceUnitTests {
+
+    @Autowired
+    private ItemService itemService;
+
+    @MockBean
+    private ItemRepository itemRepository;
+
+    @BeforeEach
+    public void setup() {
+
+    }
+
+    @Test
+    public void testCreateUpdatePriceOnItem_ValidCreating() {
+
+    }
+
+    @Test
+    public void testCreateUpdatePriceOnItem_ValidUpdating() {
+
+    }
+
+    @Test
+    public void testCreateUpdatePriceOnItem_InvalidItem() {
+
+    }
+
+    @Test
+    public void testCreateUpdatePriceOnItem_InvalidPrice() {
+
+    }
+
+    @Test
+    public void testGetPricesOfItem_Valid() {
+
+    }
+
+    @Test
+    public void testGetPricesOfItem_InvalidItem() {
+
+    }
 }
